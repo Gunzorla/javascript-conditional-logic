1c7a82653e3d6d68d1ff82c0a43293bc83105298
Gunzorla <g.gun.n.1991@gmail.com>
2024-09-09 18:15:14 +0700

    Add Exercise 1-7

--- ex-6.js
+++ ex-6.js
@@ -1,14 +1,17 @@
-let favoriteBooks = [];
+let favoriteBooks = []
 
 function addFavoriteBook(bookName) {
-  // Start coding here !
+  if (bookName.includes("Beauty")) {
+    favoriteBooks.push(bookName)
+    return
+  }
 }
 
 // Example case
-addFavoriteBook("Let Me Tell You What I Mean");
-addFavoriteBook("Life Among the Terranauts");
-addFavoriteBook("The Beauty of Living Twice");
-addFavoriteBook("Black Beauty");
-addFavoriteBook("Monstrous Beauty");
+addFavoriteBook("Let Me Tell You What I Mean")
+addFavoriteBook("Life Among the Terranauts")
+addFavoriteBook("The Beauty of Living Twice")
+addFavoriteBook("Black Beauty")
+addFavoriteBook("Monstrous Beauty")
 
-console.log(favoriteBooks);
+console.log(favoriteBooks)
